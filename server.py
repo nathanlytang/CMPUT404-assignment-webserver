bb96dce1913eafcffda03b21eaf7b9b875ea102b
Nathan Tang <nathan.ly.tang@gmail.com>
2022-01-25 23:58:51 -0700

    Feat: Webserver supports 200, 301, 404, 405

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
+import os
+import mimetypes
+from email.utils import formatdate
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+# Copyright 2022 Abram Hindle, Eddie Antonio Santos, Nathan Tang
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,9 +33,95 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        '''
+        Handle HTTP requests and return HTTP responses
+        '''
+
+        ENCODING = 'utf-8'
+        STATUS_200 = {'code': 200, 'message': "HTTP/1.1 200 OK"}
+        STATUS_301 = {'code': 301, 'message': "HTTP/1.1 301 Moved Permanently"}
+        STATUS_404 = {'code': 404, 'message': "HTTP/1.1 404 Not Found"}
+        STATUS_405 = {'code': 405, 'message': "HTTP/1.1 405 Method Not Allowed"}
+
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        request, *options = self.data.decode(ENCODING).split("\r\n")
+        print(request)
+        method, path, version = request.split(' ')
+
+        # Only supports GET method
+        if method != 'GET':
+            response = self.send_response(STATUS_405)
+
+        # Check if path is being falsified
+        elif not os.path.realpath(f"./www/{path}").startswith(os.path.realpath("./www")):
+            response = self.send_response(STATUS_404)
+
+        # Check if dir
+        elif os.path.isdir(f"./www/{path}") and not path.endswith('/'):
+            response = self.send_response(STATUS_301, location=f"{path}/")
+
+        # If requesting a dir
+        elif os.path.isdir(f"./www/{path}") and path.endswith('/'):
+            file = open(f"./www/{path}/index.html", "r")
+            lines = file.read()
+            file.close()
+            content = {
+                'content': lines,
+                'length': len(lines.encode(ENCODING)),
+                'type': f"text/html; charset={ENCODING}"
+            }
+            response = self.send_response(STATUS_200, content=content)
+
+        # If requesting a specific file
+        elif os.path.exists(f"./www/{path}"):
+            file = open(f"./www/{path}", "r")
+            lines = file.read()
+            file.close()
+            content = {
+                'content': lines,
+                'length': len(lines.encode(ENCODING)),
+                'type': mimetypes.guess_type(f"./www/{path}")[0]
+            }
+            response = self.send_response(STATUS_200, content=content)
+
+        # Return 404 if nothing is found
+        else:
+            response = self.send_response(STATUS_404)
+
+        return self.request.sendall(bytearray(response, ENCODING))
+
+    def send_response(self, status, content = None, location = None):
+        '''
+        Build the response for the request using HTTP headers and body
+
+        Parameters
+        ---
+        status : dict
+            HTTP status with a code and message
+        content : dict
+            Content information with type, length, and body
+        location : str
+            Location path for 301 redirects
+
+        Returns
+        ---
+        response : str
+            A response string
+        '''
+        response = f"{status['message']}\r\n"
+        response += f"Server: nathanlytang/0.1\r\n"
+        response += f"Date: {formatdate(timeval=None, localtime=False, usegmt=True)}\r\n"
+        if status['code'] == 405:
+            response += f"Allow: GET\r\n"
+        # Redirect to proper location if 301
+        if location is not None:
+            response += f"Location: {location}\r\n"
+        # Add content information
+        if content is not None:
+            response += f"Content-Type: {content['type']}\r\n"
+            response += f"Content-Length: {content['length']}\r\n"
+            response += f"\r\n{content['content']}"
+        return response
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
